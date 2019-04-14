569c79dbab9af5e8147ab7e68c014b5c671a4855
Jhannn <johann.guerra200@gmail.com>
2019-04-13 23:46:59 -0300

    Adicionado Apollo-Client e um teste basico com a nova tecnologia adicionada.

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ApolloClient from 'apollo-client';
+import { HttpLink, InMemoryCache } from 'apollo-client-preset';
+import { ApolloProvider, graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <ApolloProvider client={client}>
+      <View style={{ padding: 10 }}>
+      <MovieDetails />
+    </View>
+    </ApolloProvider>
     );
   }
 }
@@ -18,4 +24,45 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+});
+
+
+// Apollo client
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'https://startups-project-mytvsxrgeb.now.sh' }),
+  cache: new InMemoryCache().restore({}),
+});
+ 
+// Example query from https://www.graph.cool/
+const MOVIE_QUERY = gql`
+query GetAllStartups {
+  allStartups {
+    name
+    teamCount
+    description
+    imageUrl
+    annualReceipt
+    Segment {
+      name
+      code
+    }
+  }
+}
+`;
+ 
+// MovieDetails Component
+const MovieDetails = graphql(MOVIE_QUERY)(({ data }) => {
+  const { loading, allStartups } = data;
+
+  if (loading) return <View><Text>loading...</Text></View>;
+
+  return (
+    <View style={{ padding: 10 }}>
+      {allStartups.map(({ name }) => (
+        <Text key={name}>
+          {name}
+        </Text>
+      ))}
+    </View>
+  );
 });
